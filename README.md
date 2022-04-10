0782849feac400fbe193c3f7149a3781fa1c2b92
sammy-jean <Sammy.jean@icloud.com>
2022-04-10 14:22:16 -0400

    initial re-commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# p2-jean-mojica-almanza-campo
+
+# Class Project Two
+
+This a a project we created its a highly interactive website in order to demonstrate our understanding of using JavaScript and APIs to manipulate the DOM. 
+
+
+## Requirements
+
+ - a. Mobile Hamburger Menu
+- b. Navigation Sub-menu Drop Down
+- c. Pop-up Ad
+- d. Slide-out Ad
+- e. Accordion
+- f. “Read More” Open and Close Button
+- g. Tabbed Content
+
+
+
+
+## Authors
+
+- [@SammyJean](https://www.github.com/Sammy-Jean)
+- [@kimberlyMojica](https://github.com/kimberlymojica)
+- [@NikiaAlmanza](https://github.com/Nikia05)
+
+
+
---
+++ index.js
@@ -0,0 +1,83 @@
+
+
+// NAV BAR JS - PUT CODE UNDER HERE
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+
+
+
+
+
+// HOME PAGE JS - PUT CODE UNDER HERE
+
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
+
+
+
+
+
+
+// ABOUT PAGE JS - PUT CODE UNDER HERE     
+
+
+
+
+
+
+
+
+
+
+// HELP PAGE JS - PUT CODE UNDER HERE
+
+     // ANIMAL PHOTO
+
+const cat_btn = document.getElementById("cat_btn");
+const dog_btn = document.getElementById("dog_btn");
+const cat_result = document.getElementById("cat_result");
+const dog_result = document.getElementById("dog_result");
+
+cat_btn.addEventListener("click", getRandomCat);
+dog_btn.addEventListener("click", getRandomDog);
+
+function getRandomCat() {
+  fetch("https://aws.random.cat/meow")
+    .then((res) => res.json())
+    .then((data) => {
+      cat_result.innerHTML = `<img src=${data.file} alt="cat" />`;
+    });
+}
+
+function getRandomDog() {
+  fetch("https://random.dog/woof.json")
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.url.includes(".mp4")) {
+        getRandomDog();
+      } else {
+        dog_result.innerHTML = `<img src=${data.url} alt="dog" />`;
+      }
+    });
+}
+
+
+
