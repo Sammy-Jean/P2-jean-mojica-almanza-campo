fa8632488b967174dffed3299026e585c67ef843
kimmojica <kimberly.mojica@ufl.edu>
2022-04-10 18:14:28 -0700

    Add read more button to about

--- index.js
+++ index.js
@@ -35,10 +35,23 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
 
 
 
-
-
-
 // ABOUT PAGE JS - PUT CODE UNDER HERE     
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
 
 
 
